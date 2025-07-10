e365b28ec39b341db1289218ab1c934ae1169d77
billy <144733159+WBattist@users.noreply.github.com>
2025-07-09 22:25:14 -0400

    Update TuffX.java

--- src/main/java/net/potato/tuff/TuffX.java
+++ src/main/java/net/potato/tuff/TuffX.java
@@ -43,53 +43,75 @@ import java.util.stream.Collectors;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
-import org.bukkit.event.EventPriority;
 
 import org.bukkit.ChunkSnapshot;
-import org.bukkit.Chunk;
 
 public class TuffX extends JavaPlugin implements Listener, PluginMessageListener, TabCompleter {
 
     public static final String CHANNEL = "eagler:below_y0";
-@@ -224,9 +232,14 @@
 
-        switch (action.toLowerCase()) {
-            case "break":
-                getLogger().info("breaking block at "+loc.getX()+","+loc.getY()+","+loc.getZ());
-                ItemStack tool = player.getInventory().getItemInMainHand();
-                if (player.getGameMode() == GameMode.SURVIVAL) {
-                    block.breakNaturally();
-                    BlockBreakEvent breakEvent = new BlockBreakEvent(block, player);
-                    getServer().getPluginManager().callEvent(breakEvent);
+    private final Set<ChunkSectionKey> sentChunks = ConcurrentHashMap.newKeySet();
 
-                    if (!breakEvent.isCancelled()) {
-                        block.breakNaturally(player.getInventory().getItemInMainHand());
-                    }
-                } else {
-                    block.setType(Material.AIR);
+    @Override
+    public void onEnable() {
+        getLogger().info("TuffX has been enabled.");
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, CHANNEL);
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, CHANNEL, this);
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("TuffX has been disabled.");
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        if (!channel.equals(CHANNEL)) {
+            return;
+        }
+
+        try (DataInputStream in = new DataInputStream(new ByteArrayInputStream(message))) {
+            String subchannel = in.readUTF();
+            if ("block_op".equals(subchannel)) {
+                String action = in.readUTF();
+                int x = in.readInt();
+                int y = in.readInt();
+                int z = in.readInt();
+                Location loc = new Location(player.getWorld(), x, y, z);
+                Block block = loc.getBlock();
+
+                switch (action.toLowerCase()) {
+                    case "break":
+                        getLogger().info("breaking block at " + loc.getX() + "," + loc.getY() + "," + loc.getZ());
+                        if (player.getGameMode() == GameMode.SURVIVAL) {
+                            BlockBreakEvent breakEvent = new BlockBreakEvent(block, player);
+                            getServer().getPluginManager().callEvent(breakEvent);
+
+                            if (!breakEvent.isCancelled()) {
+                                block.breakNaturally(player.getInventory().getItemInMainHand());
+                            }
+                        } else {
+                            block.setType(Material.AIR);
+                        }
+                        break;
+                    default:
+                        getLogger().warning("Received unknown block_op action: " + action);
+                        break;
                 }
-@@ -290,7 +303,7 @@
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private byte[] createSectionPayload(World world, int cx, int cz, int sectionY) throws IOException {
-   /* private byte[] createSectionPayload(World world, int cx, int cz, int sectionY) throws IOException {
-        try (ByteArrayOutputStream bout = new ByteArrayOutputStream(8200); DataOutputStream out = new DataOutputStream(bout)) {
-            out.writeUTF("chunk_data");
-            out.writeInt(cx);
-@@ -312,6 +325,37 @@
-            }
-            return bout.toByteArray();
-        }
-    }*/
-
-    private byte[] createSectionPayload(World world, int cx, int cz, int sectionY) throws IOException {
         Chunk chunk = world.getChunkAt(cx, cz);
-        ChunkSnapshot snapshot = chunk.getChunkSnapshot(true, false, false); // avoid lighting overhead
+        ChunkSnapshot snapshot = chunk.getChunkSnapshot(true, false, false);
 
         try (ByteArrayOutputStream bout = new ByteArrayOutputStream(8200);
-            DataOutputStream out = new DataOutputStream(bout)) {
-            
+             DataOutputStream out = new DataOutputStream(bout)) {
+
             out.writeUTF("chunk_data");
             out.writeInt(cx);
             out.writeInt(cz);
@@ -100,11 +122,9 @@ public class TuffX extends JavaPlugin implements Listener, PluginMessageListener
                 for (int z = 0; z < 16; z++) {
                     for (int x = 0; x < 16; x++) {
                         int worldY = baseY + y;
-
-                        Material type;
-                        type = snapshot.getBlockType(x, worldY, z);
+                        Material type = snapshot.getBlockType(x, worldY, z);
                         if (type == null) type = Material.AIR;
-
+                        
                         short legacyId = LegacyBlockIdManager.getLegacyShort(type);
                         out.writeShort(legacyId);
                     }
@@ -114,39 +134,43 @@ public class TuffX extends JavaPlugin implements Listener, PluginMessageListener
         }
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-@@ -416,4 +460,44 @@
-
-    private record ChunkSectionKey(UUID playerId, String worldName, int cx, int cz, int sectionY) {
+    private record ChunkSectionKey(UUID playerId, String worldName, int cx, int cz, int sectionY) {}
+    
+    private void sendBlockUpdateToNearby(Location loc, Material material) {
+        getLogger().info("Sending block update for " + material.name() + " at " + loc.toString());
     }
 
-
+    private void sendChunkSectionsAsync(Player player, Chunk chunk) {
+        getLogger().info("Sending chunk sections to " + player.getName() + " for chunk " + chunk.getX() + "," + chunk.getZ());
+    }
+    
+    private boolean isPlayerInChunkRange(Player player, Chunk chunk, int viewDistance) {
+        int dx = Math.abs(player.getLocation().getChunk().getX() - chunk.getX());
+        int dz = Math.abs(player.getLocation().getChunk().getZ() - chunk.getZ());
+        return dx <= viewDistance && dz <= viewDistance;
+    }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
         sendBlockUpdateToNearby(block.getLocation(), Material.AIR);
-        //sendChunkSectionIfBelowY0(event.getPlayer(), block);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent event) {
         Block block = event.getBlock();
         sendBlockUpdateToNearby(block.getLocation(), block.getType());
-        //sendChunkSectionIfBelowY0(event.getPlayer(), block);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onBlockPhysics(BlockPhysicsEvent event) {
         Block block = event.getBlock();
         sendBlockUpdateToNearby(block.getLocation(), block.getType());
-        //sendChunkSectionIfBelowY0(null, block);
     }
 
     private void sendChunkSectionIfBelowY0(Player player, Block block) {
-        if (block.getY() >= 0) return; 
+        if (block.getY() >= 0) return;
 
-        int sectionY = block.getY() >> 4;
         Chunk chunk = block.getChunk();
 
         if (player != null) {
